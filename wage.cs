104764b220d0844c630913953877e57a7b93293a
Lakshmi <lakshmiynh8@gmail.com>
2024-03-14 17:39:15 +0530

    adedd:dailywage file

---
+++ wage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    class wage
+    {
+        double rupie;
+        double hvr;
+        public wage(double  rupie,double hvr)
+        {
+            this.rupie = rupie;
+            this.hvr = hvr;
+        }
+
+        public double onedaywage()
+        {
+
+            return  hvr * rupie;
+        }
+    }
+}
