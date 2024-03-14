5dc7038d5e6bf0c68015885b79c3d5e956a23ab6
Lakshmi <lakshmiynh8@gmail.com>
2024-03-14 17:44:06 +0530

    adedd:one month wage

---
+++ monthwage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    class monthwage
+    {
+
+        public void calmonthwage()
+        {
+            wage ob = new wage(20, 8);
+            double onemonthwage = ob.onedaywage();
+            Console.WriteLine(onemonthwage);
+        }
+    }
+}
