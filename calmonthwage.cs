8cbcabd768bae6cce17a6049f453aa74bd77532f
Lakshmi <lakshmiynh8@gmail.com>
2024-03-14 17:46:02 +0530

    adedd:calculate monthly salary file

---
+++ calmonthwage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+
+
+    internal class calmonthwage
+    {
+        public void monthwage()
+        {
+            Console.WriteLine("Enter how many days you worked");
+            double days = Convert.ToDouble(Console.ReadLine());
+            double onemonth = 20;
+            wage obj = new wage(20, 8);
+            double w = obj.onedaywage();
+
+            if (days == onemonth)
+            {
+                double onemonthwage = w * days;
+                Console.WriteLine(onemonthwage);
+            }
+
+        }
+    }
+}
