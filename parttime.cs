083cbd1e61ac08d369336dba10cbbbbe52500b05
Lakshmi <lakshmiynh8@gmail.com>
2024-03-14 17:40:47 +0530

    adedd:partime file

---
+++ parttime.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    class parttime
+
+
+    {
+        double phvr;
+        double amt;
+        public parttime(double phvr, double amt)
+        {
+            this.phvr = phvr;
+            this.amt = amt;
+        }
+        public double addparttime()
+        {
+
+            return amt * phvr;
+
+        }
+    }
+}
