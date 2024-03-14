193670132301eabd37af9dd72bbf2244ae16f58e
Lakshmi <lakshmiynh8@gmail.com>
2024-03-14 17:33:10 +0530

    adedd:welcome file

---
+++ welcome.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+     class welcome
+    {
+        public void wel()
+        {
+            Console.WriteLine("welcome to Employee wage computation..");
+        }
+    }
+}
